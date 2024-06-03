5a55da2990e15e71a3223f9e6fdf043658015688
Dev Saraf <134863681+devsar27@users.noreply.github.com>
2024-06-03 11:13:30 +0530

    Add files via upload

---
+++ SW.js
@@ -0,0 +1,28 @@
+// const playButton = document.getElementsByClassName("play")[0];
+// const lapButton = document.getElementsByClassName("lap")[0];
+// const resetButton = document.getElementsByClassName("reset")[0];
+
+// let isPlay = false
+
+// const toggleButton = () => {
+//     lapButton.classList.remove("hidden");
+//     resetButton.classList.remove("hidden");
+// }
+
+// const play = () => {
+//     if (!isPlay) {
+//         playButton.innerHTML = 'Pause';
+//         isPlay = true;
+//     } else {
+//         playButton.innerHTML = 'Play';
+//         isPlay = false;
+//     }
+//     toggleButton();
+// }
+
+// const reset = () => {
+//     lapButton.classList.add("hidden");
+//     resetButton.classList.add("hidden");
+// }
+// playButton.addEventListener("click",play);
+// resetButton.addEventListener()
